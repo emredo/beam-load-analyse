910995ec0fa6c4fd0264c3dbe1b9c0219b5a5442
Emre <himmet172@gmail.com>
2022-06-03 12:56:21 +0300

    Safety situation under loading added.

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ while True:
     if load_end < load_start or load_start > beam_length or load_end > beam_length:
         print("\nPlease enter a valid load end point.\n")
         continue
-    #note: if user enters non float value, program raises error.
+
     try:
         load_function_a = float(input("Please enter the coefficient of x^2 : "))
         load_function_b = float(input("Please enter the coefficient of x : "))
@@ -41,10 +41,23 @@ while True:
 assert loads != [], "No correct load data, exiting!!!"
 beam = Beam(loads,beam_length,beam_section_type)
 
+beam_safety_shear_stress = int(float(input("\nPlease enter the safety shear stress in MPa: ")))
+beam_safety_tensile_stress = int(float(input("Please enter the safety tensile stress in MPa: ")))
+beam_safety_coef = int(float(input("Please enter the safety coefficient: ")))
+
 print("\nBEAM PROPERTIES\nMoment of Inertia (mm^4): {}\nArea (mm^2): {}\nBeam length (mm): {}\nSection type: {}\n".format(beam.moi,beam.section_area,beam.length,beam.section_type))
 print("\nREACTION VALUES\nReaction X in (N):\t{}\nReaction Y in (N):\t{}\nReaction Moment (N.mm):\t{}\nMaximum normal stress (N/mm^2):\t{}\nMaximum shear stress (N/mm^2):\t".format(beam.reaction_x,
                                                                             beam.reaction_y,
                                                                             beam.reaction_moment,beam.max_normal_stress,beam.max_shear_stress))
+if beam_safety_shear_stress < beam_safety_coef * beam.max_shear_stress:
+    print("\nWARNING: Maximum shear stress is higher than safety shear stress.\n")
+else:
+    print("\nMaximum shear stress is lower than safety shear stress.\n")
+
+if beam_safety_tensile_stress < beam_safety_coef * beam.max_normal_stress:
+    print("\nWARNING: Maximum normal stress is higher than safety tensile stress.\n")
+else:
+    print("\nMaximum normal stress is lower than safety tensile stress.\n")
 
 path = os.getcwd()
 path = os.path.join(path,"save_{}".format(int(time.time())))
